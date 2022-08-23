fd1f0933ee1a560d576dc7d3d71fa5d80e29a8ef
Bruno Arruda <bruno.arrm@gmail.com>
2022-08-22 22:39:07 -0300

    Comentários no arquivo de rotas para auxiliar os demais membros da equipe

--- src/components/Rotas/index.js
+++ src/components/Rotas/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+// Importar os componentes para as rotas aqui
 import Inicio from '../pages/Inicio/Inicio';
 import Login from '../pages/Login/Login';
 
 const Rotas = () => (
     <BrowserRouter>
         <Routes>
+            {/* Rota principal para a página "Início" */}
             <Route exact path='/' element={ <Inicio /> } />
+            {/* Rotas secundárias devem ser adicionadas abaixo */}
             <Route path='/Login' element={ <Login /> } />
         </Routes>
     </BrowserRouter>
