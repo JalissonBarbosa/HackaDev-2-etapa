33a5c69fc9abc30bac7a3bd0000aff97b8c7b8f6
Bruno Arruda <bruno.arrm@gmail.com>
2022-08-27 15:02:45 -0300

    Respondividade em todas as páginas foi revisada

--- src/components/pages/PassRecovery/PassRecovery.js
+++ src/components/pages/PassRecovery/PassRecovery.js
@@ -3,19 +3,21 @@ import '../../../index.css';
 import './PassRecovery.css';
 import Navbar from "../../Nav/Navbar";
 import FormPassRecovery from "../../Form/FormPassRecovery";
+import recSenhaPromoA from '../../images/recSenhaPromoA.png';
+import recSenhaPromoB from '../../images/recSenhaPromoB.png';
 
 export default function PassRecovery() {
     return (
         <div className="PRbackground">
             <Navbar id="header"/>
             <div className="PRbloco" id="PRleft">
-                <p>IMAGEM</p>
+                <img src={recSenhaPromoA} alt="recSenhaPromoA"></img>
             </div>
             <div className="PRbloco" id="PRmid">
                 <FormPassRecovery />
             </div>
             <div className="PRbloco" id="PRright">
-                <p>IMAGEM</p>
+            <img src={recSenhaPromoB} alt="recSenhaPromoA"></img>
             </div>
         </div>
     )
