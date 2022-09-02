1cc807945c77ac64302a0c7e075912992b58f6cd
Igor Leandro <igor.lrl96@gmail.com>
2022-09-01 21:50:23 -0300

    Melhoria no css

--- src/components/pages/CardComponets/InfoInicial/InfoInicial.jsx
+++ src/components/pages/CardComponets/InfoInicial/InfoInicial.jsx
@@ -4,11 +4,13 @@ import './infoinicial.css';
 
 const InfoInicial = () => {
     return (
-        <div>
+        <div className="info">
             <h1>TOP COM RECORTE EM TELA ESTAMPA DIGITAL</h1>
+            <div className="preco">
             <small>Promoção!</small>
             <s>R$49,90</s>
             <h2>R$39,90</h2>
+            </div>
         </div>
     );
 }
